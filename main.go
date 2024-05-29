5d0dfd0868b4c5a9af3a80ceeceea2f270b95468
PFrek <pedrogayaf@gmail.com>
2024-05-28 21:32:04 -0300

    Add template html response to metrics/

--- main.go
+++ main.go
@@ -19,9 +19,15 @@ func (config *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (config *apiConfig) metricsHandler(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.Header().Set("Content-Type", "text/html")
 	writer.WriteHeader(200)
-	fmt.Fprintf(writer, "Hits: %v", config.fileserverHits)
+	template := "<html>\n"
+	template += "<body>\n"
+	template += "<h1>Welcome, Chirpy Admin</h1>\n"
+	template += fmt.Sprintf("<p>Chirpy has been visited %d times!</p>\n", config.fileserverHits)
+	template += "</body>\n"
+	template += "</html>\n"
+	writer.Write([]byte(template))
 }
 
 func (config *apiConfig) resetHandler(writer http.ResponseWriter, req *http.Request) {
@@ -41,15 +47,15 @@ func main() {
 	fileserverHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/*", apiConfig.middlewareMetricsInc(fileserverHandler))
 
-	mux.HandleFunc("GET /healthz", func(writer http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(writer http.ResponseWriter, req *http.Request) {
 		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		writer.WriteHeader(200)
 		writer.Write([]byte("OK\n"))
 	})
 
-	mux.HandleFunc("GET /metrics", apiConfig.metricsHandler)
+	mux.HandleFunc("GET /admin/metrics", apiConfig.metricsHandler)
 
-	mux.HandleFunc("/reset", apiConfig.resetHandler)
+	mux.HandleFunc("/api/reset", apiConfig.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
